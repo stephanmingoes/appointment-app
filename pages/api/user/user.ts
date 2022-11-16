50cc3996a43d9799bfa4e2545ba83e9bd73f8c75
stephanminges <stephanminges@gmail.com>
2022-11-16 09:58:08 -0500

    Delete Request Fix

--- pages/api/user/user.ts
+++ pages/api/user/user.ts
@@ -1,5 +1,6 @@
 import dbConnect from "../../../lib/mongodb";
 import { ResponseData, User as UserType } from "../../../types";
+import Appointment from "../../../models/Appointment";
 import User from "../../../models/User";
 import type { NextApiRequest, NextApiResponse } from "next";
 import auth from "../../../middleware/auth";
@@ -13,24 +14,25 @@ async function handler(
     const { id } = req.body;
     switch (req.method) {
       case "DELETE":
+        await Appointment.deleteMany({
+          $or: [{ patient: id }, { doctor: id }],
+        });
         const deletedUser = await User.findByIdAndDelete(id);
         return res
           .status(200)
           .json({ message: "User deleted successfully", data: deletedUser });
       case "GET":
         const user = await User.findById(id);
-        return res
-          .status(200)
-          .json({
-            message: "User found",
-            data: {
-              name: user.name,
-              email: user.email,
-              about: user.about,
-              type: user.type,
-              birthday: user.birthday,
-            },
-          });
+        return res.status(200).json({
+          message: "User found",
+          data: {
+            name: user.name,
+            email: user.email,
+            about: user.about,
+            type: user.type,
+            birthday: user.birthday,
+          },
+        });
       default:
         return res.status(400).json({ message: "Invalid http request" });
     }
